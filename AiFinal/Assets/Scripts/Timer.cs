b4e45bf961ef753a69115fbe6e9cef98324bf6ce
WornTitan123 <keiloconnor91@gmail.com>
2018-10-29 21:48:46 -0400

    created a console only timer

---
+++ AiFinal/Assets/Scripts/Timer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    float totalTime = 0.0f;
+	
+	// Update is called once per frame
+	void Update () {
+        totalTime += Time.deltaTime;
+        Debug.Log(totalTime);
+	}
+}
