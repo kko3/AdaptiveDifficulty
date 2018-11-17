b7147297029750a9857587dd23aa17e836bce423
WornTitan123 <keiloconnor91@gmail.com>
2018-11-17 18:18:32 -0500

    turn function removed

--- AiFinal/Assets/Scripts/Player/PlayerMovement.cs
+++ AiFinal/Assets/Scripts/Player/PlayerMovement.cs
@@ -37,18 +37,7 @@ public class PlayerMovement : MonoBehaviour
 
     void Turning()
     {
-        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        RaycastHit floorHit;
-
-        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
-        {
-            Vector3 playerToMouse = floorHit.point - transform.position;
-            playerToMouse.y = 0f;
-
-            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
-            playerRigidbody.MoveRotation(newRotation);
-        }
+        
     }
 
     void Animating(float h, float v)
